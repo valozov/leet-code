8197f41350661ba004143a109aa9c4b5eaaba429
valozov <93435153+valozov@users.noreply.github.com>
2026-04-04 22:44:34 +0300

    Time: 0 ms (100%), Space: 8.5 MB (91.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long y=0,x1=x;
+        while(x1>0) {
+            y= y*10 + x1%10;
+            x1/=10;
+        }
+        return x==y;
+    }
+};
