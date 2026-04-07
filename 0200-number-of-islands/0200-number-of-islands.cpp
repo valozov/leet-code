db24a86e64f8507102d304d3b8dcc135740d7f60
valozov <93435153+valozov@users.noreply.github.com>
2026-04-07 09:49:07 +0300

    Time: 26 ms (63.31%), Space: 16.2 MB (78.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& grid, int i, int j) {
-        if (i<0 || j<0 || i >= grid.size() || j >= grid[0].size()) return false;
-        else if (grid[i][j] == '1') {
-            grid[i][j] = '0';
-            check(grid, i, j + 1);
-            check(grid, i, j - 1);
-            check(grid, i + 1, j);
-            check(grid, i - 1, j);
-            return true;
-        }
-        return false;
+    void check(vector<vector<char>>& grid, int i, int j) {
+        if (i<0 || j<0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0') return;
+        
+        grid[i][j] = '0';
+        check(grid, i, j + 1);
+        check(grid, i, j - 1);
+        check(grid, i + 1, j);
+        check(grid, i - 1, j);
     }
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) return 0;
         int ans = 0;
         for (auto i = 0; i < grid.size(); ++i) {
             for (int j = 0; j < grid[0].size(); ++j) {
