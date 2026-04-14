6ac02958f8245733dbc8391776fcb4385613b8f2
valozov <93435153+valozov@users.noreply.github.com>
2026-04-14 12:04:46 +0300

    Time: 0 ms (100%), Space: 52.4 MB (24.28%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int goal = nums.size()-1;
-        for(int i = nums.size()-2; i>=0; --i){
-            if(i+nums[i] >= goal) goal = i;
+        int i=nums.size()-2;
+        int goal=nums.size()-1;
+        while(i>=0){
+            if(i + nums[i] >= goal) goal = i;
+            --i;
         }
-        return goal ? false : true;
+    if(goal==0) return true;
+    return false;
     }
 };
